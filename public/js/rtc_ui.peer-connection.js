a5365c4e97c99fd854485d5acf9e202e23435316
zenfish <zenfish@gmail.com>
2014-04-29 19:51:27 -0700

    ...

--- public/js/rtc_ui.peer-connection.js
+++ public/js/rtc_ui.peer-connection.js
@@ -14,6 +14,7 @@ rtcMultiConnection.sdpConstraints.mandatory = {
 // using websockets for signaling!
 // https://github.com/muaz-khan/WebRTC-Experiment/tree/master/websocket-over-nodejs
 // var SIGNALING_SERVER = 'wss://wsnodejs.nodejitsu.com:443';
+var SIGNALING_SERVER = 'wss://10.1.80.1:12034';
 
 rtcMultiConnection.openSignalingChannel = function(config) {
     config.channel = config.channel || this.channel;
