5bf1b9f20e01e483004ca1dc67e6e94cae170cd2
zenfish <zenfish@gmail.com>
2014-08-25 18:44:24 -0700

    ...,.

--- f-u-openssl/rot-in-hell.sh
+++ f-u-openssl/rot-in-hell.sh
@@ -40,16 +40,16 @@ KEY_CN=$(dd if=/dev/urandom bs=16 count=1 2>/dev/null| hexdump |awk '{$1=""; pri
 magic="-subj /C=$KEY_COUNTRY/ST=$KEY_PROVINCE/L=$KEY_CITY/O=$KEY_ORG/CN=$KEY_CN"
 
 # server
-openssl req $magic -batch -nodes -new -newkey rsa:$KEY_SIZE -config stupid.conf -keyout d3ck.key -out d3ck.req -batch
-openssl ca $magic -batch -in d3ck.req -out d3ck.crt -config stupid.conf -days $KEY_LIFE -batch
+openssl req $magic -extensions server -batch -nodes -new -newkey rsa:$KEY_SIZE -config stupid.conf -keyout d3ck.key -out d3ck.req -batch
+openssl  ca $magic -extensions server -batch -in d3ck.req -out d3ck.crt -config stupid.conf -days $KEY_LIFE -batch
 
 # even more pseudo random
 KEY_CN=$(dd if=/dev/urandom bs=16 count=1 2>/dev/null| hexdump |awk '{$1=""; printf("%s", $0)}' | sed 's/ //g')
 magic="-subj /C=$KEY_COUNTRY/ST=$KEY_PROVINCE/L=$KEY_CITY/O=$KEY_ORG/CN=$KEY_CN"
 
 # client
-openssl req $magic -new -newkey rsa:$KEY_SIZE -config stupid.conf -keyout vpn_client.key -out vpn_client.req -batch -nodes
-openssl ca $magic -batch -in vpn_client.req -out vpn_client.crt -config stupid.conf -days $KEY_LIFE -batch
+openssl req $magic -extensions client -new -newkey rsa:$KEY_SIZE -config stupid.conf -keyout vpn_client.key -out vpn_client.req -batch -nodes
+openssl  ca $magic -extensions client -batch -in vpn_client.req -out vpn_client.crt -config stupid.conf -days $KEY_LIFE -batch
 
 chmod -R 755 /etc/d3ck/f-u-openssl
 
