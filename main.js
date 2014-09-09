fdbfcec75ed20b34a14bb85d59dc6386776041e5
zenfish <zenfish@gmail.com>
2014-09-09 07:14:05 -0700

    ...,.

--- main.js
+++ main.js
@@ -1264,11 +1264,11 @@ function create_cli3nt_rest (req, res, next) {
 
         var cli3nt_bundle = fs.readFileSync(d3ck_keystore +'/'+ did + "/cli3nt.json").toString()
 
-        fs.writeFile(d3ck_keystore +'/'+ did + "/_sent_cli3nt.key", cli3nt_bundle.vpn.key, function(err) {
+        fs.writeFile(d3ck_keystore +'/'+ did + "/_sent_cli3nt.key", JSON.parse(cli3nt_bundle).vpn.key, function(err) {
             if (err) { console.log('err... no status... looks bad.... gasp... choke...' + err) }
             else { console.log('wrote remote vpn server IP') }
         });
-        fs.writeFile(d3ck_keystore +'/'+ did + "/_sent_cli3nt.cert", cli3nt_bundle.vpn.cert, function(err) {
+        fs.writeFile(d3ck_keystore +'/'+ did + "/_sent_cli3nt.cert", JSON.parse(cli3nt_bundle).vpn.cert, function(err) {
             if (err) { console.log('err... no status... looks bad.... gasp... choke...' + err) }
             else { console.log('wrote remote vpn server IP') }
         });
