553874ccc8c065c76a32385539db399446b0788f
zenfish <zenfish@gmail.com>
2014-09-25 09:20:14 -0700

    2048

--- testkeys.sh
+++ testkeys.sh
@@ -14,18 +14,22 @@ key_tmp="$d3ck_home/tmp/k"
 
 local_id=$(/bin/ls -asl /etc/d3ck/d3cks/D3CK | awk -F/ '{print $NF}')
 
-remote_id=$(/bin/ls -1 /etc/d3ck/d3cks/ | awk '{ if ('"$local_id"' != $1 && length($1) == 40){ print $1; exit;}}')
+remote_id=$(/bin/ls -1 /etc/d3ck/d3cks/ | awk '{ if ("'"$local_id"'" != $1 && length($1) == 40){ print $1; exit;}}')
 
-# echo $local_id
-# echo $remote_id
+echo local:  $local_id
+echo remote: $remote_id
 
 if [ "X$remote_id" = "X" ]; then
     echo Could not get a 2nd ID, bailin out
     exit 2
 fi
 
+# path_2_CA="$key_store/$remote_id/d3ckroot.crt"
+# path_2_client="$key_store/$remote_id/cli3nt.crt"
+
 path_2_CA="$key_store/$remote_id/d3ckroot.crt"
 path_2_client="$key_store/$remote_id/cli3nt.crt"
+
 
 # echo $path_2_CA
 # echo $path_2_client
@@ -38,6 +42,7 @@ openssl verify -purpose sslclient -CAfile $path_2_CA $path_2_client
 
 echo
 echo Are the CAs the same?
+echo $path_2_CA vs. $key_store/D3CK/d3ckroot.crt
 cmp $path_2_CA $key_store/D3CK/d3ckroot.crt
 echo
 
@@ -45,8 +50,15 @@ echo fetching remote certs
 
 mkdir $key_tmp 2> /dev/null
 
+# remote_path_2_CA="$key_store/$local_id/d3ckroot.crt"
 remote_path_2_CA="$key_store/$local_id/d3ckroot.crt"
 remote_path_2_client="$key_store/$local_id/cli3nt.crt"
+
+echo getting:
+echo
+echo   $remote_path_2_CA
+echo   $remote_path_2_client
+echo
 
 scp -i $remote_key $remote_user@$remote_ip:$remote_path_2_CA     $key_tmp
 scp -i $remote_key $remote_user@$remote_ip:$remote_path_2_client $key_tmp
