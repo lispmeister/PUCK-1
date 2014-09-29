0898ef66ddacd8e176ef0adcbd766dae0d30ddbd
zenfish <zenfish@gmail.com>
2014-09-29 15:49:05 -0700

    knok

--- main.js
+++ main.js
@@ -2285,20 +2285,28 @@ function uploadSchtuff(req, res, next) {
 
                 console.log('FN: ' + target_file)
 
-                fs.createReadStream(tmpfile).pipe(request.post(url, options, function optionalCallback (err, resp) {
-                    if (err) {
-                        console.error('upload failed:', err);
-                        }
-                    else {
-                        console.log('Upload successful...!  ' + JSON.stringify(resp))
+                // fs.createReadStream(tmpfile).pipe(request.post(url, options, function optionalCallback (err, resp)
+                request.post( {
+                    headers : {'content-type' : 'application/x-www-form-urlencoded'},
+                    url     : url,
+                    key     : fs.readFileSync(d3ck_keystore +'/'+ ip2d3ck[ip] + "/cli3nt.key").toString(),
+                    cert    : fs.readFileSync(d3ck_keystore +'/'+ ip2d3ck[ip] + "/cli3nt.crt").toString(),
+                    body    : tmpfile
+                    }, function cb (err, resp) {
+                        if (err) {
+                            console.error('upload failed:', err);
+                            }
+                        else {
+                            console.log('Upload successful...!  ' + JSON.stringify(resp))
 
-                        var browser_magic          = { "notify_add":false, "notify_ring":false, "notify_file":true}
-                        d3ck_status.browser_events = browser_magic
-                        d3ck_status.file_events    = file_magic
-                        createEvent(client_ip, {event_type: "remotely_uploaded", "file_name": target_file, "file_size": target_size, "d3ck_id": ip2d3ck[upload_target], "target ip": upload_target }, d3ck_status)
-                        res.send(204, {"status" : file_name})
+                            var browser_magic          = { "notify_add":false, "notify_ring":false, "notify_file":true}
+                            d3ck_status.browser_events = browser_magic
+                            d3ck_status.file_events    = file_magic
+                            createEvent(client_ip, {event_type: "remotely_uploaded", "file_name": target_file, "file_size": target_size, "d3ck_id": ip2d3ck[upload_target], "target ip": upload_target }, d3ck_status)
+                            res.send(204, {"status" : file_name})
+                        }
                     }
-                }))
+                )
             }
         }
     }
