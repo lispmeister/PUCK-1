fdc37071809507e34f9f51d44f84a1b463a05090
zenfish <zenfish@gmail.com>
2014-09-29 20:27:41 -0700

    knok

--- c2.js
+++ c2.js
@@ -40,9 +40,9 @@ var my_did = 'C73D67BF1BBED09067882B37CD5D6CC424A9F396'
             cert         : fs.readFileSync(d3ck_keystore +'/'+ d3ckid + "/d3ck.crt").toString(),
             // 'x-filename' : target_file,
             // 'x-filesize' : target_size,
-            'X-FILENAME' : 'i.jpg',
-            'X-FILESIZE' : 11813,
-            'X-D3CKID'   : my_did,
+            'X_FILENAME' : 'i.jpg',
+            'X_FILESIZE' : 11813,
+            'X_D3CKID'   : my_did,
             my_file      : fs.createReadStream('i.jpg')
         };
 
