6d7930d88b3c45c682b1039fe32bc01c2f7508e5
zenfish <zenfish@gmail.com>
2014-10-09 17:15:14 -0700

    ...

--- main.js
+++ main.js
@@ -3430,7 +3430,7 @@ server.get('/ping/:key', auth, echoStatus)
 
 // knock knock proto to request access to a system that doesn't trust you
 server.post('/knock', auth, knockKnock);
-server.post('/knock/:did/:answer', auth, knockReply);
+server.get('/knock/:did/:answer', auth, knockReply);
 
 server.post('/vpn/start', auth, startVPN);
 
