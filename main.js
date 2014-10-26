0ab36cbf00529a6fcc29d013b46e0794a782d532
zenfish <zenfish@gmail.com>
2014-10-26 13:44:33 -0700

    ...

--- main.js
+++ main.js
@@ -1073,7 +1073,7 @@ function getGeo(req, res, next) {
 
     var ip = req.query.ip
 
-    var geo = resolve_geo([ip])
+    var geo = resolve_geo(ip)
 
     res.send(200, '{"geo" : ' + geo + '}');
 
