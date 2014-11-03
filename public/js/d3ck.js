fca0a3851e57edde58dbdc017f9854333475fa04
zenfish <zenfish@gmail.com>
2014-11-03 10:17:57 -0800

    ...

--- public/js/d3ck.js
+++ public/js/d3ck.js
@@ -2064,7 +2064,7 @@ function ask_user_4_response(data) {
         var friend = req.from
 
         // var message_request = '<h2>' + req.from + '</h2> wants to connect from <span style="font-weight: 600">' + req.ip_addr + '</span><br /><span style="font-weight:100">' + req.from_d3ck + '</span><br />'
-        var message_request = '<span><img style="float: left; height:64px" src="' + all_d3ck_ids[req.from_d3ck].image + '">' +
+        var message_request = '<span><img style="float: left; height:64px; border-radius:50%;" src="' + all_d3ck_ids[req.from_d3ck].image + '">' +
                               '<h2 style="position: relative;">' + req.from + '</h2></span><br />'
 
         $("#labels", function () {
@@ -2158,7 +2158,8 @@ function show_user_sequence(d3ckid) {
     //  return
     //
 
-    var message_request = '<h2>Calling...</h2><br />'
+    var message_request = '<h2 style="position: relative;">Calling</h2>' +
+                          '<img style="float: left; height:64px; border-radius:50%;" src="' + all_d3ck_ids[d3ckid].image + '"></span><br />'
 
     $("#labels", function () {
         alertify.set({
